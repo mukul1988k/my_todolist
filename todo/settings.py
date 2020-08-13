1325aa63539b7a2e936548db4508f5c6c5445258
Mukesh Kumar <mukul1988.kumar@live.com>
2020-08-13 23:06:41 +0530

    Our first commit

--- todo/settings.py
+++ todo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '#g%&6qyn4h+%zsn&^b@&ooj@&%blfn5t*7h$p6td%@(8aq)bt$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'mukeshkr.herokuapp.com']
 
 
 # Application definition
